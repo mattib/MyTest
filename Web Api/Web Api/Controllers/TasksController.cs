66503d2922a237fe50ea8525e9f1476964d38af7
Matti Ben-Avraham <matti@Ben-Avraham.info>
2013-04-26 13:53:17 +0300

    Added the Web Api First update

---
+++ Web Api/Web Api/Controllers/TasksController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Web_Api.Models;
+
+namespace Web_Api.Controllers
+{
+    public class TasksController : ApiController
+    {
+        Task[] tasks = new Task[] 
+        { 
+            new Task { Id = 1, UserId = 1, CompanyId = 1, PickupAddress = "Moddin" }, 
+            new Task { Id = 2, UserId = 1, CompanyId = 1, PickupAddress = "Tel Aviv" }, 
+            new Task { Id = 3, UserId = 1, CompanyId = 1, PickupAddress = "Jerusalem" } 
+        };
+
+        public IEnumerable<Task> GetAllTasks()
+        {
+            return tasks;
+        }
+
+        public Task GetTaskById(int id)
+        {
+            var task = tasks.FirstOrDefault((p) => p.Id == id);
+            if (task == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+            return task;
+        }
+
+        public IEnumerable<Task> GetTasksByUser(int userId)
+        {
+            return tasks.Where( (p) => int.Equals(p.UserId, userId));
+        }
+
+
+        //for test
+        public IEnumerable<Task> GetTasksByAddress(string address)
+        {
+            return tasks.Where((p) => string.Equals(p.PickupAddress, address, StringComparison.OrdinalIgnoreCase));
+        }
+    }
+}
---
+++ Web Api/Web Api/Models/Task.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web_Api.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int CompanyId { get; set; }
+        public string PickupName { get; set; }
+        public string DeliverypName { get; set; }
+        public string PickupAddress { get; set; }
+        public string DeliveryAddress { get; set; }
+        public DateTime? PickUpTime { get; set; }
+        public DateTime? DeliveryTime { get; set; }
+        public string PickupContactPerson { get; set; }
+        public string DelieryContactPerson { get; set; }
+        public string PickupContactPhoneNumber { get; set; }
+        public string DelieryContactPhoneNumber { get; set; }
+        public bool Delivered { get; set; }
+        public bool? Rejected { get; set; }
+        public string RejectionReason{ get; set; }
+        public int Status { get; set; }
+    }
+}
